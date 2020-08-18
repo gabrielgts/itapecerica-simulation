d66be10ed2a2f84a99cc0110e985027b810bb008
Gabriel Silveira <gabriel@e-flips.com.br>
2020-08-18 00:18:32 -0300

    Fixed iqa formula

--- iqa.py
+++ iqa.py
@@ -6,27 +6,36 @@ import numpy as np
 dataset = pd.read_csv("parametros_ssinal_trasformed.csv", encoding='utf8',
                       delimiter=';', engine='python')
 
-print(dataset.head())
-
-dataset.info()
-
-dataset['Turbidez'].astype('float32')
+dataset['Data de Amostragem'] = pd.to_datetime(dataset['Data de Amostragem'])
 
 print(dataset.columns.tolist())
 
 dataset = dataset.fillna(0)
 
-dataset['IQA'] = (dataset['Oxigênio dissolvido'] * 0.17) + \
-    (dataset['Coliformes termotolerantes'] * 0.15) + \
-    (dataset['pH laboratório'] * 0.12) + \
-    (dataset['Demanda Bioquímica de Oxigênio'] * 0.1) + \
-    ((dataset['Nitrogênio amoniacal total'] + dataset['Nitrogênio orgânico']) * 0.1) + \
-    (dataset['Fósforo total'] * 0.1) + \
-    (dataset['Temperatura da água'] * 0.1) + \
-    (dataset['Turbidez'] * 0.08) + \
-    (dataset['Sólidos totais'] * 0.08)
+dataset['IQA'] = (dataset['Oxigenio dissolvido'].astype('float32') ** 0.17) * \
+    (dataset['Coliformes termotolerantes'].astype('float32') ** 0.15) * \
+    (dataset['pH laboratorio'].astype('float32') ** 0.12) * \
+    (dataset['Demanda Bioquimica de Oxigenio'].astype('float32') ** 0.1) * \
+    ((dataset['Nitrogenio amoniacal total'].astype('float32') + dataset['Nitrogenio organico'].astype('float32')) ** 0.1) * \
+    (dataset['Fosforo total'].astype('float32') ** 0.1) * \
+    (dataset['Temperatura da agua'].astype('float32') ** 0.1) * \
+    (dataset['Turbidez'].astype('float32') ** 0.08) * \
+    (dataset['Solidos totais'].astype('float32') ** 0.08)
 
-print(dataset['IQA'])
+newDataset = pd.DataFrame({
+    'IQA': dataset['IQA'],
+    'oxigenio_dissolvido': dataset['Oxigenio dissolvido'].astype('float32'),
+    'coliformes_termotolerantes': dataset['Coliformes termotolerantes'].astype('float32'),
+    'pH_laboratorio': dataset['pH laboratorio'].astype('float32'),
+    'demanda_bioquimica_oxigenio': dataset['Demanda Bioquimica de Oxigenio'].astype('float32'),
+    'nitrogenio_total': dataset['Nitrogenio amoniacal total'].astype('float32') + dataset['Nitrogenio organico'].astype('float32'),
+    'fosforo_total': dataset['Fosforo total'].astype('float32'),
+    'temperatura_da_agua': dataset['Temperatura da agua'].astype('float32'),
+    'turbidez': dataset['Turbidez'].astype('float32'),
+    'solidos_totais': dataset['Solidos totais'].astype('float32'),
+    'data': dataset['Data de Amostragem']
+})
 
+newDataset = newDataset.sort_values(by='data')
 
-dataset['IQA'].to_csv('normalized_data_iqa.csv', encoding='utf-8')
+newDataset.to_csv('iqa_.csv', encoding='utf-8')
